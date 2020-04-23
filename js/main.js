86e884e9f490558841136f8f438def5b53dd088e
Mohit Patel <3443mohit@gmail.com>
2020-04-23 05:33:13 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// const logo = document.querySelectorAll(".banner-heading path");
+// console.log(logo);
+// for(let i=0; i<logo.length; i++) {
+// 	console.log(''+i+' '+logo.length+' '+logo[i].getTotalLength());
+// }
